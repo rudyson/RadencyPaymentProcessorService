ef36a88f774373d011b9afd0afab1806206017b8
Ruslan <contact@xnrudyson.anonaddy.me>
2023-02-21 12:32:07 +0200

    Reading configuration from App.config

--- RadencyPaymentProcessorService/Service1.cs
+++ RadencyPaymentProcessorService/Service1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Diagnostics;
 using System.Linq;
@@ -12,6 +13,8 @@ namespace RadencyPaymentProcessorService
 {
     public partial class Service1 : ServiceBase
     {
+        private string input_source_path = string.Empty;
+        private string output_source_path = string.Empty;
         public Service1()
         {
             InitializeComponent();
@@ -19,10 +22,16 @@ namespace RadencyPaymentProcessorService
 
         protected override void OnStart(string[] args)
         {
+            this.ReadConfiguration();
         }
 
         protected override void OnStop()
         {
+        }
+        private void ReadConfiguration()
+        {
+            input_source_path = ConfigurationManager.AppSettings["SourcePath"];
+            output_source_path = ConfigurationManager.AppSettings["ProcessedDataPath"];
         }
     }
 }
